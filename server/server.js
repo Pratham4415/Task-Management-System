5ca0faf08d5909ad53edf3d6a89de7ab5b12fa8c
Pratham Patel <patelpratham028@gmail.com>
2026-09-07 18:41:15 +0530

    Configure Vercel deployment settings

--- server/server.js
+++ server/server.js
@@ -50,3 +50,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Export for Vercel serverless functions
+module.exports = app;
